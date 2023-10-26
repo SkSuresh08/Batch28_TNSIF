31c5b3958af1d1a76a26d1b3868c552a9c318756
SkSuresh08 <148351757+SkSuresh08@users.noreply.github.com>
2023-10-26 14:16:32 +0530

    Add files via upload

---
+++ First_program.java
@@ -0,0 +1,7 @@
+public class First_program
+{
+   public static void main(String[] args)
+   {
+     System.out.print("Name : SureshKumar\nReg_no : 730220104039\nDept_name : BE.CSE\nClg_name : Al-Ameen Engineering College" );
+   }
+}
